6f70da452d7b5a019d00bcf8f7b68f23af985593
ahalsey <xandros@linux.com>
2019-03-24 01:13:08 -0600

    Add files via upload

--- countingemails.py
+++ countingemails.py
@@ -2,10 +2,12 @@
 #
 import sqlite3
 
-connection = sqlite3.connect('emails.sqlite')
+connection = sqlite3.connect('orgs.sqlite')
 cursor = connection.cursor()
 
-cursor.execute('create table Counts (org text, count integer)')
+cursor.execute('drop table if exists Counts')
+
+cursor.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
 
 interations = 0
 fname = input('Enter file name: ')
@@ -14,17 +16,23 @@ for line in fhandle:
     if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1]
-    cursor.execute('select count from Counts where org = ? ', (org,))
+    pieces = email.split("@")
+    org = pieces[1]
+    cursor.execute('SELECT count from Counts where org = ? ', (org,))
     row = cursor.fetchone()
     if row is None:
-        cursor.execute('insert into Counts (org, count) values (?,1)', (org,))
+        cursor.execute('INSERT into Counts (org, count) values (?,1)', (org,))
     else:
-        cursor.execute('update Counts set count = count + 1 where email = ?', (org,))
+        cursor.execute('UPDATE Counts set count = count + 1 where org = ?', (org,))
     iterations = interations + 1
     if iterations % 10 == 0:
         connection.commit()
 
-sequel = 'select org, count from Counts order by count'
+connection.commit()
+
+sequel = 'SELECT org, count from Counts order by count desc'
 
 for row in cursor.execute(sequel):
     print(str(row[0]), row[1])
+
+cursor.close()
